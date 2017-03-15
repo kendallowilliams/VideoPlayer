01402dfd2a2b0efb05b0e42767de2e0eebbbb202
Kendall O. Williams <kendallowilliams@outlook.com>
2017-03-14 22:37:45 -0400

    VideoPlayer
    - continuing refactor.

--- html/js/video_player.js
+++ html/js/video_player.js
@@ -1,15 +1,17 @@
 function VideoPlayer () {
     var self = this,
-        domain = "http://192.168.1.100",
-        fileUtility = "/cgi-bin/get_file.cgi",
-        dataUtility = "/cgi-bin/video_player_cs.sh", /* _cs for debugging */
+        domain = "http://192.168.1.100/",
+        fileUtility = domain + "cgi-bin/get_file.cgi?id=",
+        dataUtility = "cgi-bin/video_player_cs.sh", /* _cs for debugging */
         getSeriesHttpJSON = { type: "series" },
         getSeasonsHttpJSON = { type: "season" },
         getEpisodesHttpJSON = (_series, _season) => ({ type: "episode", series: _series, season: _season }),
-        VIEWABLE_COUNT = 7;
+        VIEWABLE_COUNT = 7,
+        SKIP_SECONDS = 20;
 
     this.SCREENS = { browser: 0, video: 1, invalid: 99 };
     this.BROWSER_SCREENS = { series: 0, seasons: 1, episodes: 2, invalid: 99, edges: { min: 0, max: 2 } };
+    this.VIDEO = () => document.getElementById("video");
     
     this.VIEWS = {
         seriesHeader: () => document.querySelector(".series_header"),
@@ -105,7 +107,7 @@ function VideoPlayer () {
         "currentScreen" : {
             get: () => this._currentScreen || self.SCREENS.browser,
             set: _value => {
-                this._currentScreen = _value === self.SCREENS.video ? self.SCREENS.browser : self.SCREENS.video;
+                this._currentScreen = _value;
                 self.showCurrentScreen();
             }
         },
@@ -117,6 +119,36 @@ function VideoPlayer () {
                 this._currentBrowserScreen = _value;
                 self.switchBrowserScreen();
             }
+        },
+        "volume" : {
+            get: () => this._volume || 10.0,
+            set: _value => {
+                var volume = _value;
+                
+                if (_value < 0) /*then*/ volume = 0;
+                else if (_value > 10.0) /*then*/ volume = 10.0;
+                
+                this._volume = volume;
+                self.VIDEO().volume = this._volume / 10.0;
+            }
+        },
+        "currentTime": {
+            get: () => self.VIDEO().currentTime,
+            set: _value => {
+                var duration = self.VIDEO().duration;
+                
+                if (_value > duration) /*then*/ self.VIDEO().currentTime = duration;
+                else if (_value < 0) /*then*/ self.VIDEO().currentTime = 0;
+                else /*then*/ self.VIDEO().currentTime = _value;
+            }
+        },
+        "autoplay": {
+            get: () => this._autoplay,
+            set: _value => this._autoplay = _value
+        },
+        "playing": {
+            get: () => this._playing,
+            set: _value => this._playing = _value
         }
     });
     
@@ -124,6 +156,7 @@ function VideoPlayer () {
         this.initialized = false;
         this.loadData();
         this.initializeControls();
+        this.initializePlayer();
         this.showCurrentScreen();
         this.initialized = true;
     };
@@ -145,18 +178,39 @@ function VideoPlayer () {
             _item().onmouseout = _evt => _evt.currentTarget.parentElement.setAttribute("vp-hover", "");
         });
         
-        this.CONTROL_BUTTONS.centerButton().onclick = () => self.currentScreen = self.currentScreen;
+        this.CONTROL_BUTTONS.centerButton().onclick = () => self.currentScreen = self.currentScreen === self.SCREENS.video ? self.SCREENS.browser : self.SCREENS.video;
         
         this.CONTROL_BUTTONS.leftButton().onclick = () => {
             if (self.currentScreen === self.SCREENS.browser) /*then*/ self.currentBrowserScreen--;
-            else if (self.currentScreen === self.SCREENS.video) /*then*/ null;
+            else if (self.currentScreen === self.SCREENS.video) /*then*/ self.currentTime -= SKIP_SECONDS;
         };
         
         this.CONTROL_BUTTONS.rightButton().onclick = () => {
             if (self.currentScreen === self.SCREENS.browser) /*then*/ self.currentBrowserScreen++;
-            else if (self.currentScreen === self.SCREENS.video) /*then*/ null;
+            else if (self.currentScreen === self.SCREENS.video) /*then*/ self.currentTime += SKIP_SECONDS;
         };
 
+        this.CONTROL_BUTTONS.upButton().onclick = () => {
+            if (self.currentScreen === self.SCREENS.browser) /*then*/ self.scrollBrowserScreen(-1);
+            else if (self.currentScreen === self.SCREENS.video) /*then*/ self.volume++;
+        };
+        
+        this.CONTROL_BUTTONS.downButton().onclick = () => {
+            if (self.currentScreen === self.SCREENS.browser) /*then*/ self.scrollBrowserScreen(1);
+            else if (self.currentScreen === self.SCREENS.video) /*then*/ self.volume--;
+        };
+    };
+    
+    this.initializePlayer = function () {
+        var player = this.VIDEO();
+        
+        player.onended = function () {
+            self.playNextEpisode();
+        };
+        
+        player.onerror = function () {
+            self.playNextEpisode();
+        };
     };
     
     this.showCurrentScreen = function () {
@@ -176,21 +230,73 @@ function VideoPlayer () {
     };
     
     this.switchBrowserScreen = function () {
+        var browser = null,
+            header = null,
+            footer = null;
+        
         this.resetBrowserViews();
         switch(self.currentBrowserScreen) {
             case this.BROWSER_SCREENS.series:
-                this.showSeries();
+                browser = this.VIEWS.seriesBrowser();
+                header = this.VIEWS.seriesHeader();
+                footer = this.VIEWS.seriesFooter();
                 break;
             case this.BROWSER_SCREENS.seasons:
-                this.showSeasons();
+                browser = this.VIEWS.seasonsBrowser();
+                header = this.VIEWS.seasonsHeader();
+                footer = this.VIEWS.seasonsFooter();
                 break;
             case this.BROWSER_SCREENS.episodes:
-                this.showEpisodes();
+                browser = this.VIEWS.episodesBrowser();
+                header = this.VIEWS.episodesHeader();
+                footer = this.VIEWS.episodesFooter();
                 break;
             case this.SCREENS.invalid:
             default:
                 break;
         }
+        
+        if (browser && header && footer) {
+            browser.setAttribute("vp-visible", "1");
+            header.setAttribute("vp-visible", "1");
+            footer.setAttribute("vp-visible", "1");
+        }
+    };
+    
+    this.scrollBrowserScreen = function (_direction) {
+        var startIndex = 0,
+            count = 0,
+            direction = VIEWABLE_COUNT * _direction;
+        
+        switch(self.currentBrowserScreen) {
+            case this.BROWSER_SCREENS.series:
+                this.showSeries();
+                break;
+            case this.BROWSER_SCREENS.seasons:
+                startIndex = parseInt(this.VIEWS.seasonsBrowser().getAttribute("startIndex"));
+                count = parseInt(this.VIEWS.seasonsBrowser().getAttribute("seasonCount"));
+                
+                if (startIndex + direction < 0) /*then*/ startIndex = 0;
+                else if (startIndex + direction >= count) /*then*/ startIndex = startIndex;
+                else startIndex = startIndex + direction;
+                    
+                this.showSeasons(this.VIEWS.seasonsBrowser().getAttribute("seriesID"), count, startIndex);
+                break;
+            case this.BROWSER_SCREENS.episodes:
+                startIndex = parseInt(this.VIEWS.episodesBrowser().getAttribute("startIndex"));
+                count = parseInt(this.VIEWS.episodesBrowser().getAttribute("episodeCount"));
+                
+                if (startIndex + direction < 0) /*then*/ startIndex = 0;
+                else if (startIndex + direction >= count) /*then*/ startIndex = startIndex;
+                else startIndex = startIndex + direction;
+                    
+                this.loadEpisodes(this.episodeData, startIndex);
+                this.VIEWS.episodesBrowser().setAttribute("startIndex", startIndex);
+                break;
+            case this.SCREENS.invalid:
+            default:
+                break;
+        };
     };
     
     this.getSeries = function () {
@@ -200,63 +306,70 @@ function VideoPlayer () {
             .then(this.showSeries);
     };
     
-    this.loadSeries = function (_data) {
-        while(this.VIEWS.seriesBrowser().firstChild)
-        {
-            this.VIEWS.seriesBrowser().removeChild(this.VIEWS.seriesBrowser().firstChild);
-        }
+    this.loadSeries = function (_data, _start = 0) {
+        var seriesBrowser = this.VIEWS.seriesBrowser();
+        
+        seriesBrowser.setAttribute("seriesCount", _data.length);
+        while(seriesBrowser.firstChild) /*loop*/ seriesBrowser.removeChild(seriesBrowser.firstChild);
         
         _data.forEach((_item, _index) => {
-            var series = document.getElementById("series_template_div").cloneNode(true);
+            var series = document.getElementById("series_template_div").cloneNode(true),
+                count = seriesBrowser.querySelectorAll(".series_template[vp-visible='1']").length;
             series.id = "";
             series.textContent = _item.title;
             series.setAttribute("seriesID", _item.id);
             series.setAttribute("seasonsCount", _item.seasons);
             series.onclick = _evt => self.showSeasons(_evt.currentTarget.getAttribute("seriesID"),
                                                      _evt.currentTarget.getAttribute("seasonsCount"));
-            if (_index < VIEWABLE_COUNT) /*then*/ series.setAttribute("vp-visible", "1");
-            this.VIEWS.seasonsBrowser().setAttribute("seriesID", _item.id);
-            this.VIEWS.seriesBrowser().appendChild(series);
+            if (count < VIEWABLE_COUNT && _index >= _start) /*then*/ series.setAttribute("vp-visible", "1");
+            seriesBrowser.appendChild(series);
         });
     };
     
     this.showSeries = function () {
-        self.resetBrowserViews();
-        self.VIEWS.seriesBrowser().setAttribute("vp-visible", "1");
-        self.VIEWS.seriesHeader().setAttribute("vp-visible", "1");
-        self.VIEWS.seriesFooter().setAttribute("vp-visible", "1");
+        self.currentBrowserScreen = self.BROWSER_SCREENS.series;
     };
     
     this.getEpisodes = function (_series, _season) {
         var path = domain + dataUtility;
         HTTP.get(path, getEpisodesHttpJSON(_series, _season))
             .then(_value => self.episodeData = _value)
-            .then(this.showEpisodes);
+            .then(this.showEpisodes)
+            .then(() => { 
+                if (self.playing) /*then*/ self.VIEWS.episodesBrowser().firstElementChild.click()
+            });
     };
     
-    this.loadEpisodes = function (_data) {
-        while(this.VIEWS.episodesBrowser().firstChild)
-        {
-            this.VIEWS.episodesBrowser().removeChild(this.VIEWS.episodesBrowser().firstChild);
-        }
+    this.loadEpisodes = function (_data, _start = 0) {
+        var episodesBrowser = this.VIEWS.episodesBrowser();
+        
+        episodesBrowser.setAttribute("episodeCount", _data.length);
+        episodesBrowser.setAttribute("startIndex", _start);
+        while(episodesBrowser.firstChild) /*loop*/ episodesBrowser.removeChild(episodesBrowser.firstChild);
         
         _data.forEach((_item, _index) => {
-            var episode = document.getElementById("episode_template_div").cloneNode(true);
+            var episode = document.getElementById("episode_template_div").cloneNode(true),
+                count = episodesBrowser.querySelectorAll(".episode_template[vp-visible='1']").length;
             episode.id = "";
             episode.textContent = _item.title;
+            episode.setAttribute("episodeID", _item.id);
             episode.setAttribute("seriesID", _item.series_id);
             episode.setAttribute("seasonID", _item.season_id);
-            episode.onclick = _evt => null;
-            if (_index < VIEWABLE_COUNT) /*then*/ episode.setAttribute("vp-visible", "1");
-            this.VIEWS.episodesBrowser().appendChild(episode);
+            episode.onclick = _evt => {
+                var template = _evt.currentTarget.parentElement.querySelector(".episode_template[vp-playing='1']");
+                if (template) /*then*/ template.setAttribute("vp-playing", "0");
+                _evt.currentTarget.setAttribute("vp-playing", "1");
+                self.VIDEO().src = fileUtility + _evt.currentTarget.getAttribute("episodeID");
+                self.currentScreen = self.SCREENS.video;
+                self.playing = true;
+            };
+            if (count < VIEWABLE_COUNT && _index >= _start) /*then*/ episode.setAttribute("vp-visible", "1");
+            episodesBrowser.appendChild(episode);
         });
     };
     
     this.showEpisodes = function () {
-        self.resetBrowserViews();
-        self.VIEWS.episodesBrowser().setAttribute("vp-visible", "1");
-        self.VIEWS.episodesHeader().setAttribute("vp-visible", "1");
-        self.VIEWS.episodesFooter().setAttribute("vp-visible", "1");
+        self.currentBrowserScreen = self.BROWSER_SCREENS.episodes;
     };
     
     this.getSeasons = function () {
@@ -266,10 +379,9 @@ function VideoPlayer () {
     };
     
     this.loadSeasons = function (_data) {
-        while(this.VIEWS.seasonsBrowser().firstChild)
-        {
-            this.VIEWS.seasonsBrowser().removeChild(this.VIEWS.seasonsBrowser().firstChild);
-        }
+        var seasonsBrowser = this.VIEWS.seasonsBrowser();
+        
+        seasonsBrowser.setAttribute("startIndex", "0");
         
         _data.forEach((_item, _index) => {
             var season = document.getElementById("season_template_div").cloneNode(true);
@@ -283,22 +395,30 @@ function VideoPlayer () {
                 self.VIEWS.episodesBrowser().setAttribute("seriesID", seriesID);
                 self.VIEWS.episodesBrowser().setAttribute("seasonID", seasonID);
             };
-            this.VIEWS.seasonsBrowser().appendChild(season);
+            
+            seasonsBrowser.appendChild(season);
         });
     };
     
-    this.showSeasons = function (_series, _count) {
-        var seasons = this.VIEWS.seasonsBrowser().querySelectorAll(".season_template");
-        
-        this.resetBrowserViews();
-        this.VIEWS.seasonsBrowser().setAttribute("vp-visible", "1");
-        this.VIEWS.seasonsHeader().setAttribute("vp-visible", "1");
-        this.VIEWS.seasonsFooter().setAttribute("vp-visible", "1");
+    this.showSeasons = function (_series, _count, _start = 0) {
+        var browser = this.VIEWS.seasonsBrowser(),
+            seasons = browser.querySelectorAll(".season_template"),
+            visibleSeasons = browser.querySelectorAll(".season_template[vp-visible='1']");
+    
+        visibleSeasons.forEach(_item => _item.setAttribute("vp-visible", "0"));
+        self.currentBrowserScreen = self.BROWSER_SCREENS.seasons;
+        browser.setAttribute("seriesID", _series);
+        browser.setAttribute("startIndex", _start);
         
         if (_series && _count) {
+            browser.firstElementChild.setAttribute("vp-hidden", "1");
+            browser.setAttribute("seasonCount", _count);
+            
             seasons.forEach((_item, _index) => {
-                if (_index < VIEWABLE_COUNT && _index < _count) {
-                   _item.setAttribute("vp-visible", "1") 
+                var count = browser.querySelectorAll(".season_template[vp-visible='1']").length;
+                
+                if (count < VIEWABLE_COUNT && _index < _count && _index >= _start) {
+                   _item.setAttribute("vp-visible", "1");
                 }
             });
         }
@@ -314,6 +434,24 @@ function VideoPlayer () {
         this.VIEWS.seasonsBrowser().setAttribute("vp-visible", "0");
         this.VIEWS.seasonsHeader().setAttribute("vp-visible", "0");
         this.VIEWS.seasonsFooter().setAttribute("vp-visible", "0");
+    };
+    
+    this.playNextEpisode = function () {
+        var episodesBrowser = this.VIEWS.episodesBrowser(),
+            currentEpisode = episodesBrowser.querySelector(".episode_template[vp-playing='1']"),
+            nextEpisode = currentEpisode ? currentEpisode.nextElementSibling : null;
+        
+        self.playing = true;
+        if (nextEpisode && !nextEpisode.isSameNode(episodesBrowser.lastElementChild)) /*then*/ nextEpisode.click();
+        else {
+            var seasonsBrowser = this.VIEWS.seasonsBrowser(),
+                currentSeason = seasonsBrowser.querySelector(".season_template[seasonID='" + currentEpisode.getAttribute("seasonID") + "']"),
+                nextSeason = currentSeason ? currentSeason.nextElementSibling : null;
+        
+            if (nextSeason.isSameNode(seasonsBrowser.firstElementChild)) /*then*/ nextSeason = nextSeason.nextElementSibling;
+            if (nextSeason) /*then*/ nextSeason.click();
+            else /*then*/ self.playing = false;
+        }
     };
     
     this.initializeVideoPlayer();
